2b5bb08ab3e713394a8772b8273345bb3dca64aa
easedeath <easedeath@gmail.com>
2021-12-08 21:10:12 +0530

    minor changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,10 @@
 #--------CUSTOM FUNCTIONS---------------
-
 function bluers
     systemctl restart bluetooth.service
 end
 
 function com
-    g++ $argv[1] && ./a.out && rm a.out 
+    g++ $argv[1] && ./a.out 
 end
 
 
@@ -57,6 +56,8 @@ set fish_color_error '#BF616A'
 set fish_color_param '#88C0D0'
 set fish_color_keyword '#D8DEE9'
 
+set ANDROID_SDK '/home/heks/Android/Sdk'
+
 #----------------------------- ALIASES --------------------------------
 
 #----DIRECTORY CHANGE
@@ -72,12 +73,9 @@ alias ls='exa -F --color=always --group-directories-first' # my preferred listin
 alias la='exa -a --color=always --group-directories-first'  # all files and dirs
 alias ll='exa -l --color=always --group-directories-first'  # long format
 alias lt='exa -aT --color=always --group-directories-first' # tree listing
-#alias tmux="TERM=screen-256color-bce tmux" #base16 colors
 
 # pacman and yay
-alias docker='sudo docker'
 alias pacman='sudo pacman'
-alias yay=paru
 alias pacsyu='sudo pacman -Syyu'                 # update only standard pkgs
 alias yaysua='paru -Sua --noconfirm'              # update only AUR pkgs
 alias yaysyu='paru -Syu --noconfirm'              # update standard pkgs and AUR pkgs
@@ -90,12 +88,12 @@ alias df='df -h'                          # human-readable sizes
 alias free='free -m'                      # show sizes in MB
 
 #-----MISC
-
+#nvm install 16.0.0
 alias ew='~/Downloads/eww/target/release/eww'
 
 
 alias fetch='~/Public/fm6000 -c=bright_red -n -m=7 -g=7 -l=16'
-alias cf='rm -rf ~/CP && mkdir ~/CP' #competetive coding :)
+alias cf='rm -rf ~/CP && mkdir ~/CP && git init ~/CP' #competetive coding :)
 alias loff='xrandr --output eDP1 --off' #turns off laptop screen :)
 alias lonn='xrandr --output eDP1 --auto'
 #-----GREP COLOR
@@ -103,11 +101,6 @@ alias grep='grep --color=auto'
 colorscript -e crunchbang-mini 
 
 starship init fish | source
-export PATH="$HOME/.emacs.d/bin:$PATH"
+# export PYTHONDONTWRITEBYTECODE=1
+#export PATH="$HOME/.emacs.d/bin:$PATH"
 
-
-#--------JAYDA KA BACha----------
-#alias qb64="bash /home/puffc0rn/Downloads/qb64/run_qb64.sh"
-alias fl="cd /home/puffc0rn/ZZZ/JAY/Github/10_FLASK_PROJECTS/Project_10_Online_Shopping_Site;export FLASK_DEBUG=1;flask run"
-alias qb64="bash /home/strawhat54/Downloads/qb64/run_qb64.sh"
-alias eww="./eww"
